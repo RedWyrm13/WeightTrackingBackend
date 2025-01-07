69a19946085472ede8a9a6f052e04cb9fd74a4ea
RedWyrm13 <Andrewmaciejunes@yahoo.com>
2025-01-07 12:38:59 -0500

    Added KTOR dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,12 @@ repositories {
 }
 
 dependencies {
+    implementation("io.ktor:ktor-server-core:2.3.4")
+    implementation("io.ktor:ktor-server-netty:2.3.4")
+    implementation("com.google.oauth-client:google-oauth-client:1.34.1")
+    implementation("com.google.http-client:google-http-client-gson:1.34.1")
+    implementation("io.ktor:ktor-server-call-logging:2.3.4")
+
     testImplementation(kotlin("test"))
 }
 
